868c2816797280a2f58896add27dc54e09d34433
lunaa <lunaaurelio03@gmail.com>
2023-08-26 20:38:11 -0300

    agregue algunos JS y ordene css

--- sprint 1/javascript/convertidor.js
+++ sprint 1/javascript/convertidor.js
@@ -1,19 +1,30 @@
-function  convertir() {
+var boton = document.querySelector(".boton-form");
+var resultadoTexto = document.getElementById("resultado-texto");
+
+boton.addEventListener("click", function() {
     var valor = parseInt(document.getElementById("valor").value);
-    var resultado = 0;
-    var dolar = 750;
+    var resultado;
+    var dolar = 350;
     var euro = 380;
+    var libra = 440; // Agrega el valor de conversión para la libra
+
     if (document.getElementById("uno").checked){
         resultado = valor / dolar;
         resultado = parseFloat(resultado);
-        alert("El cambio a Dolares es: $" + resultado.toFixed(2));
+        resultadoTexto.textContent = "El cambio a Dólares es: $" + resultado.toFixed(3);
     }
     else if (document.getElementById("dos").checked) {
         resultado = valor / euro;
         resultado = parseFloat(resultado);
-        alert("El cambio a Euros es: €" + resultado.toFixed(2));
+        resultadoTexto.textContent = "El cambio a Euros es: €" + resultado.toFixed(3);
+    }
+    else if (document.getElementById("tres").checked) {
+        resultado = valor / libra;
+        resultado = parseFloat(resultado);
+        resultadoTexto.textContent = "El cambio a Libras es: £" + resultado.toFixed(3);
     }
     else {
-        alert("Tienes que completar todos los campos");
+        resultadoTexto.textContent = "Tienes que completar todos los campos";
     }
-}
+});
+
