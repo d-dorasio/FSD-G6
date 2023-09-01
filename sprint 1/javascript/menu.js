26511aa1cb097cbadb6ab8131af33b4625623664
camilaromero330 <camilaromero3300@gmail.com>
2023-09-01 07:59:55 -0300

    Agregada la funcionalidad de moverse con flechitas en el menu

---
+++ sprint 1/javascript/menu.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const linkList = document.getElementById('nav');
+    const links = nav.getElementsByTagName('a');
+    let focusedLinkIndex = 0;
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+        case 'ArrowUp':
+            if (focusedLinkIndex > 0) {
+            focusedLinkIndex--;
+            }
+            break;
+        case 'ArrowDown':
+            if (focusedLinkIndex < links.length - 1) {
+            focusedLinkIndex++;
+            }
+            break;
+        default:
+            return;
+        }
+
+        for (let i = 0; i < links.length; i++) {
+        links[i].classList.remove('focused');
+        }
+
+        links[focusedLinkIndex].classList.add('focused');
+        links[focusedLinkIndex].focus();
+    });
+    });
