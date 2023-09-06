f0f0a35835b1691958059c580e664947e85b8eaa
Delfina Dorasio <delfinadorasio@gmail.com>
2023-09-05 21:55:05 -0300

    componentes iniciales funcionando

---
+++ sprint 2/sprint2react/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+        <h1>Esto es el footer</h1>
+    </div>
+  )
+}
+
+export default footer
---
+++ sprint 2/sprint2react/src/components/nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <div>
+        <h1>Esto es el nav</h1>
+    </div>
+  )
+}
+
+export default nav
