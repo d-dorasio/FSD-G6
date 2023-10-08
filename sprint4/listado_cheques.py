5dbba5636aca8bbb5aa29205ce128a86e0f6259c
lunaa <lunaaurelio03@gmail.com>
2023-10-08 16:32:43 -0300

    validaciones

--- sprint4/listado_cheques.py
+++ sprint4/listado_cheques.py
@@ -3,19 +3,13 @@ from datetime import datetime
 
 # Función para cargar los cheques desde el archivo CSV
 def readFile(filecsv):
-    checks = []
-
     while True:
         try:
             with open(filecsv, mode='r', newline='') as file:
-                reader = csv.DictReader(file)
-                checks = list(reader)
-            return checks
-        
+                return list(csv.DictReader(file))
         except FileNotFoundError:
             print(" * El archivo CSV no se encontró.")
             filecsv = input("Ingrese un nuevo nombre de archivo CSV correctamente: " )
-            filecsv += ".csv"
     
 
 # Función para filtrar los cheques según diferentes criterios
@@ -41,10 +35,26 @@ def exportCsv(filtered_checks, outputfile):
         writer.writeheader()
         writer.writerows(filtered_checks)
 
+# Función para Verificar si el DNI existe en el archivo CSV
+def is_valid_dni(dni, checks):
+    for check in checks:
+        if dni == check['DNI']:
+            return True
+    return False
+
+# Función para verificar si una cadena de fecha dada cumple con el formato "aaaa-mm-dd
+def is_valid_date(date_str):
+    try:
+        if len(date_str) == 10:  # Verifica que la cadena tenga la longitud correcta (aaaa-mm-dd)
+            datetime.datetime.strptime(date_str, "%Y-%m-%d")
+            return True
+    except ValueError:
+        pass
+    return False
+
 # Función principal del programa
 def main():
-    filecsv = input("Ingrese el nombre del archivo CSV: ")
-    filecsv += ".csv"
+    filecsv = input("Ingrese el nombre del archivo CSV (con el .csv): ")
     checks = readFile(filecsv)
 
     while True:
@@ -52,14 +62,45 @@ def main():
         print("1. Ingreso de datos")
         print("4. Salir")
 
-        option = int(input("Elija una opción:"))
+        option = int(input("Elija una opción: "))
         
         if option == 1:
-            dni = input("Ingrese el DNI del cliente para la consulta: ")
-            tipo = input("Ingrese el tipo de cheque a consultar (emitido, depositado): ").lower()
-            estado = input("* OPCIONAL: Ingrese el estado del cheque a filtrar (pendiente, aprobado, rechazado): ").lower()
-            fecha_inicio = input("* OPCIONAL: Ingrese la fecha de inicio a filtrar (aaaa-mm-dd): ")
-            fecha_fin = input("* OPCIONAL: Ingrese la fecha final a filtrar (aaaa-mm-dd): ")
+            print(" * Atención! Si alguno de los datos no fueron ingresados correctamente, el programa no cargará ningún dato.")
+            
+            # Validación para el DNI
+            while True:
+                dni = input("Ingrese el DNI del cliente para la consulta: ")
+                if dni.isdigit() and is_valid_dni(dni, checks):
+                    break
+                else:
+                    print(f'* El DNI debe contener solo números y ser válido según el archivo "{filecsv}": ')
+            
+            # Validación para el tipo de cheque
+            while True:
+                tipo = input("Ingrese el tipo de cheque a consultar (emitido, depositado): ").lower()
+                if tipo in ("emitido", "depositado"):
+                    break 
+                else:
+                    print(" * El tipo de cheque debe ser 'emitido' o 'depositado'. Por favor, ingréselo correctamente: ")
+             
+            # Validación para el Estado
+            while True:
+                estado = input(" * OPCIONAL: Ingrese el estado del cheque a filtrar (pendiente, aprobado, rechazado): ")
+                if estado in ("pendiente", "aprobado", "rechazado", "", " "):
+                    break 
+                else:
+                    print(" El estado debe ser 'aprobado', 'rechazo', 'pendiente' o deje en blanco para omitir. Por favor, ingréselo correctamente: ")
+            
+            # Validación para la fecha de inicio y fecha de fin
+            while True:
+                fecha_inicio = input(" * OPCIONAL: Ingrese la fecha de inicio a filtrar (aaaa-mm-dd): ")
+                fecha_fin = input(" * OPCIONAL: Ingrese la fecha final a filtrar (aaaa-mm-dd): ")
+                
+                if (not fecha_inicio or is_valid_date(fecha_inicio)) and (not fecha_fin or is_valid_date(fecha_fin)):
+                    break
+                else:
+                    print(" Ingresó un formato de fecha incorrecto! Utilice el formato aaaa-mm-dd o deje en blanco para omitir.")
+
 
             filtered_checks = filterAll(checks, dni, tipo, estado, fecha_inicio, fecha_fin)
             salida = input('Ingrese si desea recibir la salida por "pantalla" o "csv": ').lower()
@@ -82,8 +123,10 @@ def main():
             elif salida == "csv":
                 outputfile = f"checks_{dni}.csv"
                 exportCsv(filtered_checks, outputfile)
-                print(f"resultados exportados a {outputfile}.")
-            
+                print(f"Resultados exportados a {outputfile}.")
+            else:
+                print(" * Hubo un error al elegir el tipo de salida. Por favor, intentelo de nuevo.")
+        
         elif option == 4:
             break
 
