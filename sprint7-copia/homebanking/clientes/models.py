f08f52469b7ff36df8643c0bf17652ddc2a0ff51
Delfina Dorasio <delfinadorasio@gmail.com>
2023-11-22 10:45:38 -0300

    intento inicio sesión

---
+++ sprint7-copia/homebanking/clientes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Cliente(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=255)
+    customer_surname = models.CharField(max_length=255)
+    customer_dni = models.CharField(max_length=20, db_column='customer_DNI')
+    dob = models.DateField(blank=True, null=True)
+    branch_id = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True,)
+
+    class Meta:
+        managed = True
+        db_table = 'cliente'
+
