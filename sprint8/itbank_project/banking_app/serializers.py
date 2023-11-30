8799b08ef3ad275ae7b866885dd49fbac519e5c7
lunaa <lunaaurelio03@gmail.com>
2023-11-29 23:56:04 -0300

    empezamos sprint8

---
+++ sprint8/itbank_project/banking_app/serializers.py
@@ -0,0 +1,51 @@
+# banking_app/serializers.py
+from rest_framework import serializers
+from .models import Tipo_Cliente, Cliente, Tipo_Cuenta, Cuenta, Prestamo, Sucursal, Marca_Tarjeta, Tarjeta
+from django.contrib.auth.models import User
+
+class TipoClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tipo_Cliente
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ClienteSerializer(serializers.ModelSerializer):
+    usuario = UserSerializer()
+
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+class TipoCuentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tipo_Cuenta
+        fields = '__all__'
+
+class CuentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cuenta
+        fields = '__all__'
+
+class PrestamoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prestamo
+        fields = '__all__'
+
+class SucursalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sucursal
+        fields = '__all__'
+
+class MarcaTarjetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Marca_Tarjeta
+        fields = '__all__'
+
+class TarjetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tarjeta
+        fields = '__all__'
---
+++ sprint8/itbank_project/banking_app/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+# Create your views here.
+from rest_framework import viewsets
+from .models import Tipo_Cliente, Cliente, Tipo_Cuenta, Cuenta, Prestamo, Sucursal, Marca_Tarjeta, Tarjeta
+from .serializers import TipoClienteSerializer, ClienteSerializer, TipoCuentaSerializer, CuentaSerializer, PrestamoSerializer, SucursalSerializer, MarcaTarjetaSerializer, TarjetaSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class TipoClienteViewSet(viewsets.ModelViewSet):
+    queryset = Tipo_Cliente.objects.all()
+    serializer_class = TipoClienteSerializer
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class TipoCuentaViewSet(viewsets.ModelViewSet):
+    queryset = Tipo_Cuenta.objects.all()
+    serializer_class = TipoCuentaSerializer
+
+class CuentaViewSet(viewsets.ModelViewSet):
+    queryset = Cuenta.objects.all()
+    serializer_class = CuentaSerializer
+
+class PrestamoViewSet(viewsets.ModelViewSet):
+    queryset = Prestamo.objects.all()
+    serializer_class = PrestamoSerializer
+
+class SucursalViewSet(viewsets.ModelViewSet):
+    queryset = Sucursal.objects.all()
+    serializer_class = SucursalSerializer
+
+class MarcaTarjetaViewSet(viewsets.ModelViewSet):
+    queryset = Marca_Tarjeta.objects.all()
+    serializer_class = MarcaTarjetaSerializer
+
+class TarjetaViewSet(viewsets.ModelViewSet):
+    queryset = Tarjeta.objects.all()
+    serializer_class = TarjetaSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def obtener_datos_cliente(request):
+    cliente = request.user.cliente  # Obtén el cliente asociado al usuario autenticado
+    serializer = ClienteSerializer(cliente)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def obtener_saldo_cuenta(request):
+    cliente = request.user.cliente
+    saldo = cliente.cuenta.balance
+    return Response({'saldo': saldo})
